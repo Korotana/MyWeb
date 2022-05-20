0a595f5524c831aeb7f04e4213bc0e50fb7d80a5
YUVRAJ KOROTANA <YUVRAJKOROTANA1999@GMAIL.COM>
2022-05-19 19:01:18 -0600

    Slideshow caraosel working and header done

---
+++ js/home/home.js
@@ -0,0 +1,45 @@
+{ /* <script> */ }
+var slideIndex = 0;
+var timer;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    slideIndex += n - 1;
+    clearTimeout(timer);
+    showSlides(slideIndex);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    slideIndex = n - 1;
+    clearTimeout(timer);
+    showSlides(slideIndex);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    timer = setTimeout(showSlides, 5000);
+}
+
+function clear(timer) {
+    clearTimeout(timer);
+}
